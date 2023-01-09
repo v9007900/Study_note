cb781a69c609a948060c9d68d156f8643575fe56
v9007900 <1103553344@qq.com>
2023-01-10 05:44:54 +0800

    0

--- xmake.lua
+++ xmake.lua
@@ -1,14 +1,16 @@
 add_rules("mode.debug", "mode.release")
 
--- target("Hook")                             -- 默认配置
---     set_kind("binary")                         -- exe程序
---     add_files("string/*.cpp")                   -- 编译目标目录
+target("string")                             -- 默认配置
+    set_kind("binary")                         -- exe程序
+    add_files("string/*.cpp")                   -- 编译目标目录
 
-target("Hook")                             -- 默认配置
-    set_kind("shared")                         -- exe程序
-    add_files("U_Hook/*.cpp")                   -- 编译目标目录
-    add_files("U_Hook/imGui/*.cpp") 
-    add_files("U_Hook/include/*.cpp") 
+
+
+-- target("Hook")                             -- 默认配置
+--     set_kind("shared")                         -- exe程序
+--     add_files("U_Hook/*.cpp")                   -- 编译目标目录
+--     add_files("U_Hook/imGui/*.cpp") 
+--     add_files("U_Hook/include/*.cpp") 
 
     
     -- add_linkdirs("E:/Git/UnrealEngine/Engine/Source/ThirdParty/Windows/DirectX/Lib/x64/") -- 链接库目录
