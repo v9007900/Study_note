33d9dd5c8f945d6d9bba80d2e543eecf1fe22177
v9007900 <1103553344@qq.com>
2023-01-09 17:31:43 +0800

    1

--- History/三元运算符.cpp
+++ History/三元运算符.cpp
@@ -5,13 +5,11 @@ using namespace std;
 int main()
 {
 
-    int aa{100}, bb{100}, cc{100};
-    int *ptr{&aa};
-    
+    int  aa {100}, bb {100}, cc {100};
+    int *ptr {&aa};
+
 
     *ptr = 50 < bb ? *ptr + 50 : *ptr + -50;
-//   (条件)           成立 返回+  不成立返回-    相当于 if
+    //   (条件)           成立 返回+  不成立返回-    相当于 if
     cout << *ptr;
 }
-
-
